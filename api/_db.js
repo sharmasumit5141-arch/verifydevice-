19e471cc5696dfaffdad555dce3c2693226e65b6
sharmasumit5141-arch <sharmasumit5141@gmail.com>
2026-05-08 13:39:41 +0530

    Create _db.js

---
+++ api/_db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+let client;
+let db;
+
+export async function connectDB() {
+  if (db) return db;
+  if (!client) {
+    client = new MongoClient(uri);
+    await client.connect();
+  }
+  db = client.db(process.env.DB_NAME || "captcha_db");
+  return db;
+}
